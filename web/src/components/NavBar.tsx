53283a93130c56d07670bb0323df548f74dfac3c
rohaan malik <rohaanmalik7@gmail.com>
2021-04-12 23:30:12 +0530

    add the hook for ssr

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import { Button } from "@chakra-ui/react";
+import { isServer } from "../utils/isServer";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
   let body = null;
     console.log(useMeQuery());
   // data is loading
