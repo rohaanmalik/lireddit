b6e1d36ba46735a441b06646f305ae1098e7e66c
rohaan malik <rohaanmalik7@gmail.com>
2021-09-02 21:20:42 -0400

    added load-more button

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,10 +3,10 @@ import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
-import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10
     }
@@ -14,26 +14,30 @@ const Index = () => {
     return (
       <Layout>
         <Flex align="center">
-          <Heading>
-          LiReddit
-          </Heading>
-        <NextLink href="/create-post">
-          <Link ml="auto"> create post </Link>
-        </NextLink>
+          <Heading>LiReddit</Heading>
+          <NextLink href="/create-post">
+            <Link ml="auto"> create post </Link>
+          </NextLink>
         </Flex>
-        {!data ? (
+        {!data && fetching ? (
           <div>...loading</div>
         ) : (
           <Stack>
-          {data.posts.map((p) => 
-            <Box key={p.id} p={5} shadow="md" borderWidth="1px" >
-            <Heading fontSize="xl">{p.title}</Heading>
-            <Text mt={4}>{p.textSnippet}</Text>
-            </Box>
-          )}
-          
+            {data!.posts.map((p) => (
+              <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text mt={4}>{p.textSnippet}</Text>
+              </Box>
+            ))}
           </Stack>
         )}
+        {data ? (
+          <Flex>
+            <Button isLoading={fetching} m="auto" my={8}>
+              load more
+            </Button>
+          </Flex>
+        ) : null}
       </Layout>
     );
 };
